890c5c7a6b5f056c2e3736a1e5ca3b2542a6a9ef
Dima <dmitriiburiachek@gmail.com>
2022-07-07 15:39:04 +0300

    created views for serializers

--- delivery_app/delivery_app/settings.py
+++ delivery_app/delivery_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ecommerce',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES': (
+#         'rest_framework.renderers.JSONRenderer',
+#     )
+# }
